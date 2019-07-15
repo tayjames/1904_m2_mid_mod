c92ced2b6f8f3d1810673dbe17f592faf0e8cac5
Tay <tay.james.d@gmail.com>
2019-07-14 20:05:51 -0600

    Adds average pages class method

--- app/models/author.rb
+++ app/models/author.rb
@@ -3,4 +3,9 @@ class Author < ApplicationRecord
   has_many :books, through: :author_books
 
   validates_presence_of :name
+
+  def average_pages
+    binding.pry
+    self.books.average(:pages)
+  end
 end
