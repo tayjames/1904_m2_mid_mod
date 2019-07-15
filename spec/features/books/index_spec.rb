523ccf089aea9d8450da2bb6205106c9120dded1
Tay <tay.james.d@gmail.com>
2019-07-14 20:04:03 -0600

    Edits Books index_spec

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -1,17 +1,10 @@
 require "rails_helper"
 
-# As a user,
-# When I visit '/books'
-# Then I see each book in the system including the book's:
-#   - title
-#   - number of pages
-#   - publication year
-#   - the name of each author that wrote the book
-
-RSpec.describe 'Book Index Page' do
+RSpec.describe 'Books Index Page' do
   describe 'As a user' do
     before :each do
       @murakami = Author.create!(name: "Haruki Murakami")
+
       @hard_boiled = Book.create!(title: "Hard Boiled Wonderland and the End of the World", pages: 400, publication: 1991)
       @sheep_chase = Book.create!(title: "A Wild Sheep Chase", pages: 300, publication: 1989)
     end
@@ -19,6 +12,7 @@ RSpec.describe 'Book Index Page' do
   describe 'When I visit /books'
     it "I see each book in the system including the books: title, num. of pages, pub. year, and name of each author that wrote the book" do
       visit '/books'
+      save_and_open_page
       within "#book-#{@hard_boiled.id}" do
       expect(page).to have_content(@hard_boiled.title)
       expect(page).to have_content(@hard_boiled.pages)
